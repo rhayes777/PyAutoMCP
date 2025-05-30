5f65da89269d3efaa47438c27b2a91c581a8a834
Richard <richard@rghsoftware.co.uk>
2025-05-30 15:28:20 +0100

    remove aggregator script

--- scripts/aggregator.py
+++
@@ -1,9 +0,0 @@
-from automcp import aggregate
-from mcp.server.fastmcp import FastMCP
-
-mcp = FastMCP("output_inspector")
-
-aggregate.add_tools(mcp)
-
-if __name__ == "__main__":
-    mcp.run(transport="stdio")
